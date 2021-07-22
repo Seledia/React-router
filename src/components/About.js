8d395b6e0550a78fe6bbb7f8412926fdc1cbbb82
Seledia <celia.salaslopez@gmail.com>
2021-07-22 18:40:00 +0200

    Se completa la aplicación, se añade router y el navbar

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const About = () => {
+	return (
+		<div className ='ui raise very padded text container segment'
+		style = {{marginTop:'80px'}}>
+		<h3 className = 'ui header'> About </h3>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique neque sed tincidunt ullamcorper. Donec ac condimentum ipsum, vitae bibendum neque. Nunc vehicula scelerisque diam, in dapibus ante. Duis fringilla pharetra suscipit 
+		</p>
+		</div>
+	)
+
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Contact = () => {
+	return (
+		<div className ='ui raise very padded text container segment'
+		style = {{marginTop:'80px'}}>
+		<h3 className = 'ui header'> Contact </h3>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique neque sed tincidunt ullamcorper. Donec ac condimentum ipsum, vitae bibendum neque. Nunc vehicula scelerisque diam, in dapibus ante. Duis fringilla pharetra suscipit 
+		</p>
+		</div>
+	)
+
+}
+
+export default Contact;
